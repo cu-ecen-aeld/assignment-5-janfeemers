1e1ffb045eeed0c4dfb37f8d617046db6f3c499e
Jan Feemers <jan.feemers@siemens.com>
2023-11-26 18:11:19 +0100

    feat: use modified repository

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c55a4cf0d80339964ed1d702dbe05af310a05ac0
+AESD_ASSIGNMENTS_VERSION = d495232871194f1fadbbf2674e5ca815b0b69555
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
